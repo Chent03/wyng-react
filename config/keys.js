202a2c122ea61809ac74059d50284d17c0ee66ae
Tony Chen <tc56007n@pace.edu>
2018-12-02 12:44:23 -0500

    remove logs

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    console.log('PRODDDDD')
     module.exports = require('./prod');
 } else {
-    console.log('ASDASDADSADS')
     module.exports = require('./dev');
 }
